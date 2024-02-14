2287ec131173dde90030f55c96c2dd57245993c9
Brandon Coles <brandonjc24@gmail.com>
2024-02-14 12:48:01 -0500

    Connected input to spectrum analyzer

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -175,6 +175,9 @@ void SimpleMBCompAudioProcessor::prepareToPlay (double sampleRate, int samplesPe
         buffer.setSize(spec.numChannels, samplesPerBlock);
     }
 
+    leftChannelFifo.prepare(samplesPerBlock);
+    rightChannelFifo.prepare(samplesPerBlock);
+
 }
 
 void SimpleMBCompAudioProcessor::releaseResources()
@@ -267,6 +270,9 @@ void SimpleMBCompAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
         buffer.clear (i, 0, buffer.getNumSamples());
 
     updateState();
+
+    leftChannelFifo.update(buffer);
+    rightChannelFifo.update(buffer);
 
     applyGain(buffer, inputGain);
 
